3cc3aaa1e892096fe26ec993c1fbb14f80fc1a06
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-01-23 00:05:35 +0500

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 import { SignUpDto } from './Dto/signup.dto';
 import { LogInDto } from './Dto/login.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { User } from './schemas/user.schema';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -14,14 +15,14 @@ export class AuthController {
     @Post('/signup')
     @ApiOperation({ summary: 'User registration', description: 'Register a new user' })
     @ApiBody({ type: SignUpDto })
-    @ApiResponse({ status: 201, description: 'User registered', type: () => ({ token: String }) })
+    @ApiResponse({ status: 201, description: 'User registered', type: User })
     signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
         return this.authService.signUp(signUpDto);
     }
 
     @Post('/login')
     @ApiOperation({ summary: 'User login', description: 'Authenticate and generate a token for the user' })
-    @ApiResponse({ status: 200, description: 'User authenticated', type: () => ({ token: String }) })
+    @ApiResponse({ status: 200, description: 'User authenticated', type:User })
     @ApiResponse({ status: 401, description: 'Unauthorized' })
     login(@Body() Logindto: LogInDto): Promise<{ token: string }> {
         return this.authService.login(Logindto);
