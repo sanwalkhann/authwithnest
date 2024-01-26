e10781585e09e0777a3c05c4116e10672edcebd3
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-01-26 19:01:25 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-  /* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier */
 
 
 import { NestFactory } from '@nestjs/core';
@@ -17,7 +17,17 @@ async function bootstrap() {
   .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app , document)
+  SwaggerModule.setup('api', app, document, {
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
 
   await app.listen(3000);
